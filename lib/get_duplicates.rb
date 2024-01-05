aca16e673f8c45ba2ff5f043211b1f142169efc2
paulmndza19 <paulmndza19@gmail.com>
2024-01-05 20:46:32 +0800

    Update get_duplicates.rb

--- lib/get_duplicates.rb
+++ lib/get_duplicates.rb
@@ -3,7 +3,7 @@ module GetDuplicates
     return [] if json_array.empty?
 
     email_group = json_array.group_by{ |json| json[:email] }
-    counts = email_group.map { |value, group| [value, group.map { |json| json[:id] }]}
+    counts = email_group.map { |key, value| [key, value.map { |json| json[:id] }]}
 
     counts.select { |key, value| value.length > 1 }
   end
